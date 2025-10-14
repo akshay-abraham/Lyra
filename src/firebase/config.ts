52caa2df4c02725e32e264c83531cbbaff89ddbe
Akshay Abraham <akshaykroobenabraham@gmail.com>
2025-10-14 23:38:14 +0000

    chore(config): move API keys to environment variables

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-1148187791-ff9d6",
   "appId": "1:702313910835:web:3bbc0cbe1759c742b5c946",
-  "apiKey": "AIzaSyD50qDi7kGoshCTRZHshqAp9n4cUvHyrGU",
+  "apiKey": process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   "authDomain": "studio-1148187791-ff9d6.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "702313910835"
